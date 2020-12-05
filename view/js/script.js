99ea478805781f7c2f31df5057165851c5b32d9c
avivatal <talshtv@gmail.com>
2020-12-05 22:58:53 +0200

    start

--- view/js/script.js
+++ view/js/script.js
@@ -1,1 +1,10 @@
 // JS code goes here
+window.contactsList = [{
+    name: 'Admin',
+    mobile: '9999999999',
+    email: 'admin@xyzcompany.com'
+  }];
+
+  function addContect(){
+      window.contactsList = [... data.name,data.mobile,data.email];
+  }
